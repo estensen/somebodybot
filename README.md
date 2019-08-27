695cf192f2418c71f5e54c13d87f566b9589b3ef
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-08-27 16:48:30 +0200

    Add bot that prints when keyword is mentioned

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # somebodybot
 Randomly select a Slack user
+
+## Install
+Go to `https://YOURORG.slack.com/services/new/bot` and create a Slack bot
+
+Copy the API token and store it in `.env`
+
+## Run
+```shell
+go run main.go
+```
+
--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/joho/godotenv"
+	"github.com/nlopes/slack"
 )
 
 func main() {
@@ -17,7 +20,42 @@ func main() {
 	token := os.Getenv("SLACKTOKEN")
 	if token == "" {
 		log.Fatal("missing Slack token env var")
-	}	
-	fmt.Println(token)
-}
+	}
 
+	api := slack.New(token)
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+Loop:
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			fmt.Print("Event Received\n")
+			switch ev := msg.Data.(type) {
+
+			case *slack.MessageEvent:
+				// info := rtm.GetInfo()
+
+				text := ev.Text
+				text = strings.TrimSpace(text)
+				text = strings.ToLower(text)
+
+				matched, _ := regexp.MatchString("someone", text)
+
+				if matched {
+					rtm.SendMessage(rtm.NewOutgoingMessage("Someone do it!", ev.Channel))
+				}
+
+			case *slack.RTMError:
+				fmt.Printf("error: %s\n", ev.Error())
+
+			case *slack.InvalidAuthEvent:
+				fmt.Printf("invalid credentials")
+				break Loop
+
+			default:
+				// do nothing
+			}
+		}
+	}
+}
