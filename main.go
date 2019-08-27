4a8f506161df39c93da1aeb9599be215f209e806
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-08-27 15:37:56 +0200

    Read Slack token env var

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, someone!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	token := os.Getenv("SLACKTOKEN")
+	if token == "" {
+		log.Fatal("missing Slack token env var")
+	}	
+	fmt.Println(token)
 }
 
