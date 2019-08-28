353330e04310b251db6c203ea6c9327d00746813
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-08-28 11:19:41 +0200

    Mention random user

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/nlopes/slack"
@@ -34,16 +36,25 @@ Loop:
 			switch ev := msg.Data.(type) {
 
 			case *slack.MessageEvent:
-				// info := rtm.GetInfo()
-
 				text := ev.Text
 				text = strings.TrimSpace(text)
 				text = strings.ToLower(text)
 
-				matched, _ := regexp.MatchString("someone", text)
+				someone, _ := regexp.MatchString("someone", text)
+				somebody, _ := regexp.MatchString("somebody", text)
 
-				if matched {
-					rtm.SendMessage(rtm.NewOutgoingMessage("Someone do it!", ev.Channel))
+				if someone || somebody {
+					users, _ := api.GetUsers()
+					var userIDs []string
+					for _, user := range users {
+						fmt.Println(user.Profile.DisplayName)
+						userIDs = append(userIDs, user.ID)
+					}
+
+					rand.Seed(time.Now().Unix())
+					randomUserID := userIDs[rand.Intn(len(userIDs))]
+					message := fmt.Sprintf("<@%s> do it!", randomUserID)
+					rtm.SendMessage(rtm.NewOutgoingMessage(message, ev.Channel))
 				}
 
 			case *slack.RTMError:
